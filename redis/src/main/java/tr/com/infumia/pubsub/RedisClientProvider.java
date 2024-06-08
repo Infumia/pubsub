689297a73989ae6226f60dbb0f3fa80c63ea6374
Hasan Demirtaş <utsukushihito@outlook.com>
2024-06-08 12:26:27 +0300

    Update RedisClientProvider.java

--- redis/src/main/java/tr/com/infumia/pubsub/RedisClientProvider.java
+++ redis/src/main/java/tr/com/infumia/pubsub/RedisClientProvider.java
@@ -6,7 +6,6 @@ import io.lettuce.core.RedisClient;
  * The interface for providing {@link RedisClient}.
  */
 public interface RedisClientProvider {
-
     /**
      * Provides the redis client for creating pub/sub connections.
      *
