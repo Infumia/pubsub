192f365bfa52f4e2aec104bda42604ff1a19a541
Hasan Demirtaş <utsukushihito@outlook.com>
2024-06-08 16:07:23 +0300

    Update tr.com.infumia.pubsub.build.publishing.gradle.kts

--- buildSrc/src/main/kotlin/tr.com.infumia.pubsub.build.publishing.gradle.kts
+++ buildSrc/src/main/kotlin/tr.com.infumia.pubsub.build.publishing.gradle.kts
@@ -51,30 +51,3 @@ publishing {
         }
     }
 }
-
-/*indra {
-    mitLicense()
-    github("infumia", "pubsub")
-    configurePublications {
-        artifactId = projectName
-
-        pom {
-            name = projectName
-            inceptionYear = "2024"
-            description = "Simplified pubsub library for Redis and various databases."
-            developers {
-                developer {
-                    name = "Hasan Demirtaş"
-                    url = "https://github.com/portlek/"
-                }
-            }
-            organization {
-                name = "Infumia"
-                url = "https://github.com/infumia/"
-            }
-        }
-    }
-    if (project.hasProperty("sign-required")) {
-        signWithKeyFromPrefixedProperties("infumia")
-    }
-}*/
